ae1d12f7f1d740ab0484cdec1834655e038559f3
cjumelin <cjumelin@protonmail.com>
2022-12-23 00:03:59 +0100

    Negative feedback loop, first step into letter digit transformation

--- soundex.ts
+++ soundex.ts
@@ -1,6 +1,9 @@
 export class Soundex {
     public encode(word: string): string {
-        return this.zeroPad(word)
+        let encoded = word.slice(0, 1)
+        if (word.length > 1)
+            encoded += "1"
+        return this.zeroPad(encoded)
     }
 
     private zeroPad(word: string): string {
