b0660e7066b432212026430953bc35c0d6b4f24c
cjumelin <cjumelin@protonmail.com>
2022-12-23 00:09:13 +0100

    Enhance expresivness, refactoring

--- soundex.ts
+++ soundex.ts
@@ -1,12 +1,19 @@
 export class Soundex {
     public encode(word: string): string {
-        let encoded = word.slice(0, 1)
-        if (word.length > 1)
-            encoded += "1"
-        return this.zeroPad(encoded)
+        return this.zeroPad(this.head(word) + this.encodeDigits(word))
     }
 
     private zeroPad(word: string): string {
         return word.padEnd(4, '0')
+    }
+
+    private head(word: string): string {
+        return word.slice(0, 1)
+    }
+
+    private encodeDigits(word: string): string {
+        if (word.length > 1)
+            return "1"
+        return ""
     }
 }
