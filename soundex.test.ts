3f4d5529f6c6b5be915f58612327480ce2105768
cjumelin <cjumelin@protonmail.com>
2022-12-23 13:24:36 +0100

    Negative feedback loop, multiple consonant transformation

--- soundex.test.ts
+++ soundex.test.ts
@@ -23,8 +23,8 @@ describe('Soundex', () => {
         expect(soundex.encode('A#')).toStrictEqual('A000')
     });
 
-    // test('replace multiple consonants with digits', () => {
-    //     expect(soundex.encode('Acdl')).toStrictEqual('A234')
-    // });
+    test('replace multiple consonants with digits', () => {
+        expect(soundex.encode('Acdl')).toStrictEqual('A234')
+    });
 
 })
